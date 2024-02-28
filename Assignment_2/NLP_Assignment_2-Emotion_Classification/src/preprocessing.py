b72d1958a5022b25f53d4c42ec31a5abc349c170
Hristo-Karagyozov <H.Karagyozov@student.rug.nl>
2024-02-28 14:08:17 +0100

    fixed some data issues, added some structure

--- Assignment_2/NLP_Assignment_2-Emotion_Classification/src/preprocessing.py
+++ Assignment_2/NLP_Assignment_2-Emotion_Classification/src/preprocessing.py
@@ -1,12 +1,20 @@
 import pandas as pd
 import string
 from nltk.corpus import stopwords
-# from tqdm import tqdm
-# from sklearn.model_selection import train_test_split
-# from keras.layers import Dense
-# from keras.models import Sequential
-# from keras import optimizers
+import csv
 
-df = pd.read_csv("F:/University/Courses/NLP/Assignments/Assignment_2/"
-                 "NLP_Assignment_2-Emotion_Classification/data/train_ready_for_WS.csv")
-print(df.head())
+
+# the data was all over the cells and could not be opened, this script puts everything in a row in the first cell
+def data_in_first_cell():
+    with open('data/raw/train_ready_for_WS.csv', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        with open('data/training.csv', 'w', encoding='utf-8') as g:
+            writer = csv.writer(g)
+            for row in reader:
+                new_row = [' '.join(row)]
+                writer.writerow(new_row)
+
+
+def load_data():
+    training_df = pd.read_csv("data/training.csv")
+    print(training_df.head(5))
