bf27e506a8342aa086e918ccb1443fb33827ad7e
Hristo-Karagyozov <H.Karagyozov@student.rug.nl>
2024-03-02 20:44:36 +0100

    removed log reg

--- Assignment_2/NLP_Assignment_2-Emotion_Classification/src/baseline/logistic_regression.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import string
-from nltk.corpus import stopwords
-from tqdm import tqdm
-from sklearn.model_selection import train_test_split
-from keras.layers import Dense
-from keras.models import Sequential
-from keras import optimizers
-
