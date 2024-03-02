df280b4f47343c3c3229605d2776e12d86224362
Hristo-Karagyozov <H.Karagyozov@student.rug.nl>
2024-03-02 13:37:10 +0100

    fixed small issues, main issue still persists

--- Assignment_2/NLP_Assignment_2-Emotion_Classification/src/baseline/base_preprocessing.py
+++ Assignment_2/NLP_Assignment_2-Emotion_Classification/src/baseline/base_preprocessing.py
@@ -40,49 +40,44 @@ def compute_doc_vectors(docs, w2v_model):
             if token in w2v_model.wv:
                 doc_vector += w2v_model.wv[token]
                 word_count += 1
-        if word_count > 0:
-            doc_vector /= word_count
+        doc_vector /= word_count
         doc_vectors.append(doc_vector)
     return np.array(doc_vectors)
 
 
 def build_model(input_shape):
     model = Sequential()
-    model.add(Dense(64, activation='relu', input_shape=input_shape))
+    model.add(Dense(16, activation='relu', input_shape=input_shape))
 
     model.add(Dense(1, activation='sigmoid'))
     return model
 
 
-# Main function
 def main():
-    # Load data
     df = load_data()
 
-    # Clean text data
+    # clean data
     df['cleaned_essay'] = df['essay'].apply(clean_text)
     print(df.head(5))
 
-    # Split the data into training and validation sets
     train_x, val_x, train_y, val_y = train_test_split(df['cleaned_essay'], df['emotion'], test_size=0.3, random_state=42)
 
-    # Train the word2vec model
+    # train the word2vec model
     word2vec_model = Word2Vec(sentences=train_x, vector_size=100, window=5, min_count=1, workers=4)
 
-    # Compute document vectors for training and validation sets
     train_x_vectors = compute_doc_vectors(train_x, word2vec_model)
     val_x_vectors = compute_doc_vectors(val_x, word2vec_model)
 
-    # Build the classification model
+    # build the classification model
     model = build_model(input_shape=(train_x_vectors.shape[1],))
 
-    # Compile the model
+    # compile the model
     model.compile(optimizer=optimizers.Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
 
-    # Train the model
-    history = model.fit(train_x_vectors, train_y, epochs=10, batch_size=32, validation_data=(val_x_vectors, val_y))
+    # train the model
+    history = model.fit(train_x_vectors, train_y)
 
-    # Evaluate the model
+    # evaluate
     loss, accuracy = model.evaluate(val_x_vectors, val_y)
     print(f'Validation Accuracy: {accuracy*100:.2f}%')
 
